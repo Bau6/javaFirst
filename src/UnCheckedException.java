12984beb4792d49e8e1037a69cb418ed9db984f5
bau6 <xojlodo4ek@mail.ru>
2024-11-30 16:39:14 +0300

    exception del 1 tast 7.6.1

--- src/UnCheckedException.java
+++ src/UnCheckedException.java
@@ -1,17 +1,38 @@
 public class UnCheckedException {
-    public static void main(String[] args) {
-        int number1 = 1;
-        int number2 = 0;
-
+//    public static void main(String[] args) {
+//        int number1 = 1;
+//        int number2 = 0;
+//
+//        try {
+//            if (number2 == 0) {
+//                throw new DivisionByZeroException("Нельзя делить на 0");
+//            }
+//            System.out.println(number1 / number2);
+//        } catch (DivisionByZeroException a) {
+//            System.out.println("ERROR " + a);
+//        } finally {
+//            System.out.println("Программа завершила свое действие");
+//        }
+//    }
+public static void main(String[] args) {
+    System.out.println("Метод Main успешно запущен");
+    method1();
+    System.out.println("Метод Main заканчивает свою работу");
+}
+    static void method1() {
         try {
-            if (number2 == 0) {
-                throw new DivisionByZeroException("Нельзя делить на 0");
-            }
-            System.out.println(number1 / number2);
-        } catch (DivisionByZeroException a) {
-            System.out.println("ERROR " + a);
-        } finally {
-            System.out.println("Программа завершила свое действие");
+            System.out.println("Первый метод передает привет!");
+            method2();
         }
+        catch (ArithmeticException err) {
+            System.out.println(err.getMessage());
+        }
+    }
+    static void method2() {
+        int x = 10;
+        int y = 0;
+        double z = x / y;
+        System.out.println( z );
+        System.out.println("Второй метод");
     }
 }
